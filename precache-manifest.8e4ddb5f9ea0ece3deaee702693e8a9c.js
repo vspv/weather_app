1d42b873c2093801858d2e5b25b7f44b5a90b1d2
vspv <seva--seva@ukr.net>
2020-05-28 16:10:41 +0300

    Updates

---
+++ precache-manifest.8e4ddb5f9ea0ece3deaee702693e8a9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78a6940738e540728769c9f9b398439d",
+    "url": "/weather_app/index.html"
+  },
+  {
+    "revision": "6c466c4fe8328ba7bcdf",
+    "url": "/weather_app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "74be27f18686716c31c7",
+    "url": "/weather_app/static/css/main.ad471c96.chunk.css"
+  },
+  {
+    "revision": "6c466c4fe8328ba7bcdf",
+    "url": "/weather_app/static/js/2.bfcc7af3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather_app/static/js/2.bfcc7af3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74be27f18686716c31c7",
+    "url": "/weather_app/static/js/main.707f01a6.chunk.js"
+  },
+  {
+    "revision": "2f9fde2a933143f72bd5",
+    "url": "/weather_app/static/js/runtime-main.0ca9a6a0.js"
+  },
+  {
+    "revision": "cc54c19b29890841267ee0750eb25a00",
+    "url": "/weather_app/static/media/polygon.cc54c19b.jpg"
+  }
+]);
